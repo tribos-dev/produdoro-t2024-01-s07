48eb7fb2e6f3281f3ecf2f44dc1278d907778439
Arthur Sarubi <arthur.sarubi@hotmail.com>
2024-07-25 08:23:24 -0300

    feat: implementa camada api

--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioAPI.java
@@ -21,5 +21,10 @@ public interface UsuarioAPI {
 	@PatchMapping(value = "/{idUsuario}")
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	void mudaStatusParaPausaCurta(@RequestHeader(name = "Authorization",required = true) String token, 
-    		@PathVariable UUID idUsuario);	
+    		@PathVariable UUID idUsuario);
+
+	@PatchMapping(value = "/{idUsuario}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	void mudaStatusParaPausaLonga(@RequestHeader(name = "Authorization",required = true) String token,
+								  @PathVariable UUID idUsuario);
 }
