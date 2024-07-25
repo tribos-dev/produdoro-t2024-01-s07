df8c4ba312a3ca92140e397d902048c99203b6f7
Arthur Sarubi <arthur.sarubi@hotmail.com>
2024-07-25 17:28:33 -0300

    feat: implementa camada api

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
@@ -5,15 +5,7 @@ import java.util.UUID;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/tarefa")
@@ -31,5 +23,9 @@ public interface TarefaAPI {
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     void defineTarefaComoAtiva(@PathVariable UUID idTarefa, 
     		@RequestHeader(name = "Authorization", required = true) String token);
-    
+
+    @DeleteMapping("/deleta-tarefa/{idTarefa}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    void deletaTarefa(@PathVariable UUID idTarefa,
+                      @RequestHeader(name = "Authorization", required = true) String token);
 }
