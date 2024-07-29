3534865f8517f7b8bd90cdc3edc156b5bc68b6b1
christianlacerdahotmail <christianlacerda@hotmail.com>
2024-07-29 10:38:24 -0300

    implementa camada domain

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/domain/Tarefa.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/domain/Tarefa.java
@@ -1,23 +1,16 @@
 package dev.wakandaacademy.produdoro.tarefa.domain;
 
-import java.util.UUID;
-
 import dev.wakandaacademy.produdoro.handler.APIException;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
 import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
-
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.http.HttpStatus;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import javax.validation.constraints.NotBlank;
+import java.util.UUID;
 
 @Builder
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -38,6 +31,7 @@ public class Tarefa {
 	private StatusTarefa status;
 	private StatusAtivacaoTarefa statusAtivacao;
 	private int contagemPomodoro;
+	private Integer posicao;
 
 	public Tarefa(TarefaRequest tarefaRequest) {
 		this.idTarefa = UUID.randomUUID();
@@ -48,6 +42,7 @@ public class Tarefa {
 		this.status = StatusTarefa.A_FAZER;
 		this.statusAtivacao = StatusAtivacaoTarefa.INATIVA;
 		this.contagemPomodoro = 1;
+		this.posicao = novaPosicao;
 	}
 
 	public void pertenceAoUsuario(Usuario usuarioPorEmail) {
@@ -67,5 +62,8 @@ public class Tarefa {
 			this.statusAtivacao = StatusAtivacaoTarefa.ATIVA;
 		}
 	}
-	
+
+	public void atualizaPosicao(int novaPosicao) {
+		this.posicao = novaPosicao;
+	}
 }
